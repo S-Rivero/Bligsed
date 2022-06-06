1e4915d5c13f939329e906f9f7036946c3578abc
S-Rivero <70220211+S-Rivero@users.noreply.github.com>
2022-06-06 14:49:58 -0300

    Retoques

--- EsqueletoNode/src/routes/index.js
+++ EsqueletoNode/src/routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const index = require('./index.routes');
 const auth = require('./auth.routes');
+const index = require('./index.routes');
 
-router.use(index);
 router.use(auth);
+router.use(index);
 
 module.exports = router;
