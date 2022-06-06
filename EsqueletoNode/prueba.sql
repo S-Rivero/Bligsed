1665ce5e4d9ec5e7e8641051ab7fb4f963245614
Nasher3296 <ignaciofernandez3296@gmail.com>
2022-06-06 15:26:45 -0300

    A

--- EsqueletoNode/prueba.sql
+++ EsqueletoNode/prueba.sql
@@ -27,110 +27,113 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `proyecto`
+-- Base de datos: 'proyecto'
 --
 CREATE DATABASE proyecto;
 USE proyecto;
 -- --------------------------------------------------------
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `DNI` varchar(11) NOT NULL,
-  `Nombre` varchar(65) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `Sexo` varchar(1) NOT NULL,
-  `Numero_de_telefono` int(11) NOT NULL,
-  `Tipo_de_usuario` tinyint(4) NOT NULL,
-  `password` char(250) NOT NULL,
-  `Fecha_de_nacimiento` date NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE 'usuarios' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'DNI' varchar(11) NOT NULL,
+  'Nombre' varchar(65) NOT NULL,
+  'username' varchar(50) NOT NULL,
+  'Sexo' varchar(1) NOT NULL,
+  'Numero_de_telefono' int(11) NOT NULL,
+  'Tipo_de_usuario' tinyint(4) NOT NULL,
+  'password' char(250) NOT NULL,
+  'Fecha_de_nacimiento' date NOT NULL,
+  PRIMARY KEY ('id')
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 --
--- Estructura de tabla para la tabla `alumno`
+-- Estructura de tabla para la tabla 'alumno'
 --
 
-CREATE TABLE `alumno` (
-  `ID` int(11) NOT NULL,
-  `ID Curso` int(11) NOT NULL,
-  `ID docente` int(11) NOT NULL,
-  `Padre` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `curso`
---
-
-CREATE TABLE `curso` (
-  `ID` int(11) NOT NULL,
-  `Nombre curso` varchar(70) NOT NULL
+CREATE TABLE 'alumno' (
+  'ID' int(11) NOT NULL,
+  'ID Curso' int(11) NOT NULL,
+  'ID docente' int(11) NOT NULL,
+  'Padre' int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `fichamedica`
+-- Estructura de tabla para la tabla 'curso'
 --
 
-CREATE TABLE `fichamedica` (
-  `DNI` varchar(11) NOT NULL,
-  `Enfermedad` varchar(200) NOT NULL,
-  `Internacion` varchar(200) NOT NULL COMMENT 'Razon por la que fue internado',
-  `Alergia` varchar(100) NOT NULL,
-  `Tratamiento medico` varchar(200) NOT NULL,
-  `Quirurjico` varchar(100) NOT NULL COMMENT 'Recibio cirujias?',
-  `Discapacidad fisica` varchar(100) NOT NULL COMMENT 'Ej rodilla destrozada no puede hacer movimientos bruscos\r\n',
-  `Vacunacion` varchar(3) NOT NULL COMMENT '1',
-  `Altura` double NOT NULL,
-  `Peso` double NOT NULL,
-  `Hospital` varchar(50) NOT NULL COMMENT 'Hospital al cual llamar',
-  `Obra social` varchar(20) NOT NULL,
-  `N de afiliado obra social` int(11) NOT NULL,
-  `Medico cabecera` varchar(50) NOT NULL,
-  `Domiciliomed` varchar(50) NOT NULL,
-  `Telefono medico` int(11) NOT NULL,
-  `Familiar responsable` int(11) NOT NULL
+CREATE TABLE 'curso' (
+  'ID' int(11) NOT NULL,
+  'Nombre curso' varchar(70) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `materias`
+-- Estructura de tabla para la tabla 'fichamedica'
 --
 
-CREATE TABLE `materias` (
-  `ID` int(11) NOT NULL,
-  `Materia` varchar(70) NOT NULL,
-  `IdCurso` int(11) NOT NULL
+CREATE TABLE 'fichamedica' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'DNI' varchar(11) NOT NULL,
+  'Enfermedad' varchar(200) NOT NULL,
+  'Internacion' varchar(200) NOT NULL COMMENT 'Razon por la que fue internado',
+  'Alergia' varchar(100) NOT NULL,
+  'Tratamiento medico' varchar(200) NOT NULL,
+  'Quirurjico' varchar(100) NOT NULL COMMENT 'Recibio cirujias?',
+  'Discapacidad fisica' varchar(100) NOT NULL COMMENT 'Ej rodilla destrozada no puede hacer movimientos bruscos\r\n',
+  'Vacunacion' varchar(3) NOT NULL COMMENT '1',
+  'Altura' double NOT NULL,
+  'Peso' double NOT NULL,
+  'Hospital' varchar(50) NOT NULL COMMENT 'Hospital al cual llamar',
+  'Obra social' varchar(20) NOT NULL,
+  'N de afiliado obra social' int(11) NOT NULL,
+  'Medico cabecera' varchar(50) NOT NULL,
+  'Domiciliomed' varchar(50) NOT NULL,
+  'Telefono medico' int(11) NOT NULL,
+  'Familiar responsable' varchar(65) NOT NULL,
+  'Telefono responsable' int(11) NOT NULL,
+  PRIMARY KEY ('id')
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `padres`
+-- Estructura de tabla para la tabla 'materias'
 --
 
-CREATE TABLE `padres` (
-  `ID` int(11) NOT NULL
+CREATE TABLE 'materias' (
+  'ID' int(11) NOT NULL,
+  'Materia' varchar(70) NOT NULL,
+  'IdCurso' int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `superusuarios`
+-- Estructura de tabla para la tabla 'padres'
 --
 
-CREATE TABLE `superusuarios` (
-  `id` int(11) NOT NULL,
-  `pago` tinyint(1) NOT NULL,
-  `fecha_creacion` date NOT NULL
+CREATE TABLE 'padres' (
+  'ID' int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla 'superusuarios'
+--
+
+CREATE TABLE 'superusuarios' (
+  'id' int(11) NOT NULL,
+  'pago' tinyint(1) NOT NULL,
+  'fecha_creacion' date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `superusuarios`
+-- Volcado de datos para la tabla 'superusuarios'
 --
 
-INSERT INTO `superusuarios` (`id`, `pago`, `fecha_creacion`) VALUES
+INSERT INTO 'superusuarios' ('id', 'pago', 'fecha_creacion') VALUES
 (1, 0, '2022-05-09'),
 (2, 0, '2022-05-03'),
 (3, 0, '2022-05-08');
@@ -138,16 +141,16 @@ INSERT INTO `superusuarios` (`id`, `pago`, `fecha_creacion`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Estructura de tabla para la tabla 'usuarios'
 --
 
 
 
 --
--- Disparadores `usuarios`
+-- Disparadores 'usuarios'
 --
 DELIMITER $$
-CREATE TRIGGER `cargadni` AFTER INSERT ON `usuarios` FOR EACH ROW INSERT INTO fichamedica (DNI) VALUES (concat(new.DNI))
+CREATE TRIGGER 'cargadni' AFTER INSERT ON 'usuarios' FOR EACH ROW INSERT INTO fichamedica (DNI) VALUES (concat(new.DNI))
 $$
 DELIMITER ;
 
@@ -156,33 +159,33 @@ DELIMITER ;
 --
 
 --
--- Indices de la tabla `curso`
+-- Indices de la tabla 'curso'
 --
-ALTER TABLE `curso`
-  ADD PRIMARY KEY (`ID`);
+ALTER TABLE 'curso'
+  ADD PRIMARY KEY ('ID');
 
 --
--- Indices de la tabla `fichamedica`
+-- Indices de la tabla 'fichamedica'
 --
-ALTER TABLE `fichamedica`
-  ADD KEY `DNI` (`DNI`),
-  ADD KEY `Familiar responsable` (`Familiar responsable`);
+ALTER TABLE 'fichamedica'
+  ADD KEY 'DNI' ('DNI'),
+  ADD KEY 'Familiar responsable' ('Familiar responsable');
 
 --
--- Indices de la tabla `materias`
+-- Indices de la tabla 'materias'
 --
-ALTER TABLE `materias`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `IdCurso` (`IdCurso`);
+ALTER TABLE 'materias'
+  ADD PRIMARY KEY ('ID'),
+  ADD KEY 'IdCurso' ('IdCurso');
 
 --
--- Indices de la tabla `padres`
+-- Indices de la tabla 'padres'
 --
-ALTER TABLE `padres`
-  ADD PRIMARY KEY (`ID`);
+ALTER TABLE 'padres'
+  ADD PRIMARY KEY ('ID');
 
 --
--- Indices de la tabla `usuarios`
+-- Indices de la tabla 'usuarios'
 --
 
 --
@@ -190,19 +193,19 @@ ALTER TABLE `padres`
 --
 
 --
--- AUTO_INCREMENT de la tabla `curso`
+-- AUTO_INCREMENT de la tabla 'curso'
 --
-ALTER TABLE `curso`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE 'curso'
+  MODIFY 'ID' int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `materias`
+-- AUTO_INCREMENT de la tabla 'materias'
 --
-ALTER TABLE `materias`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE 'materias'
+  MODIFY 'ID' int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `usuarios`
+-- AUTO_INCREMENT de la tabla 'usuarios'
 --
 
 
@@ -211,30 +214,30 @@ ALTER TABLE `materias`
 --
 
 --
--- Filtros para la tabla `materias`
+-- Filtros para la tabla 'materias'
 --
-ALTER TABLE `materias`
-  ADD CONSTRAINT `materias_ibfk_1` FOREIGN KEY (`IdCurso`) REFERENCES `curso` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE 'materias'
+  ADD CONSTRAINT 'materias_ibfk_1' FOREIGN KEY ('IdCurso') REFERENCES 'curso' ('ID') ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 
 
-CREATE TABLE `publicaciones` ( `id` INT NOT NULL AUTO_INCREMENT ,
-  `titulo` VARCHAR(20) NOT NULL ,
-  `descripcion` VARCHAR(255) NOT NULL ,
-  `autor` INT NOT NULL COMMENT 'relacionar con id usuario' ,
-  PRIMARY KEY (`id`)
+CREATE TABLE 'publicaciones' ( 'id' INT NOT NULL AUTO_INCREMENT ,
+  'titulo' VARCHAR(20) NOT NULL ,
+  'descripcion' VARCHAR(255) NOT NULL ,
+  'autor' INT NOT NULL COMMENT 'relacionar con id usuario' ,
+  PRIMARY KEY ('id')
 )ENGINE = InnoDB;
 
 
-CREATE TABLE `inasistencias` ( `id` INT NOT NULL AUTO_INCREMENT ,
-  `tipo` tinyint(1) NOT NULL ,
-  `motivo` VARCHAR(255) NOT NULL ,
-  `cantidad` INT NOT NULL,
-  `fecha` DATE NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE 'inasistencias' ( 'id' INT NOT NULL AUTO_INCREMENT ,
+  'tipo' tinyint(1) NOT NULL ,
+  'motivo' VARCHAR(255) NOT NULL ,
+  'cantidad' INT NOT NULL,
+  'fecha' DATE NOT NULL,
+  PRIMARY KEY ('id')
 )ENGINE = InnoDB;
-ALTER TABLE `inasistencias` ADD `id_us` INT NOT NULL AFTER `fecha`;
+ALTER TABLE 'inasistencias' ADD 'id_us' INT NOT NULL AFTER 'fecha';
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
