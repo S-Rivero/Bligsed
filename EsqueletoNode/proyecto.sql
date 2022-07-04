568709539f4e131aea191ac1c66ef6dce560e874
MendezLucas <70654158+MendezLucas@users.noreply.github.com>
2022-07-04 13:36:35 -0300

    Datos ampliados para notas por trimestre
    :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana: :banana:

--- EsqueletoNode/proyecto.sql
+++ EsqueletoNode/proyecto.sql
@@ -299,10 +299,14 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 INSERT INTO `curso` (`Nombre curso`) VALUES ( '7C');
 INSERT INTO `materias` (`Materia`, `IdCurso`, `profesor`) VALUES ( 'Matematicas', 1, 4), ( 'Lengua', 1, 4);
-INSERT INTO `alumno` (`ID`, `ID Curso`, `Padre` ) VALUES ( 6, 1, 5);
-INSERT INTO `notas` (`id_alum`, `Id_materia`, `nota`,`trimestre`) VALUES ( 6, 1, 1, 1), ( 6, 1, 10, 1), (6, 1, 9, 1), (6, 1, 5, 2), (6, 2, 4, 2), (6, 2, 7, 3), (6, 2, 8, 2), (6, 2, 9, 3);
+
+INSERT INTO `notas` (`id_alum`, `Id_materia`, `nota`,`trimestre`) VALUES ( 6, 1, 1, 1), ( 6, 1, 10, 1), (6, 1, 9, 1), (6, 1, 5, 1), (6, 2, 4, 1), (6, 2, 7, 1), (6, 2, 8, 1), (6, 2, 9, 1);
+INSERT INTO `notas` (`id_alum`, `Id_materia`, `nota`,`trimestre`) VALUES ( 6, 1, 1, 2), ( 6, 1, 10, 2), (6, 1, 9, 2), (6, 1, 5, 2), (6, 2, 4, 2), (6, 2, 7, 2), (6, 2, 8, 2), (6, 2, 9, 2);
+INSERT INTO `notas` (`id_alum`, `Id_materia`, `nota`,`trimestre`) VALUES ( 6, 1, 1, 3), ( 6, 1, 10, 3), (6, 1, 9, 3), (6, 1, 5, 3), (6, 2, 4, 3), (6, 2, 7, 3), (6, 2, 8, 3), (6, 2, 9, 3);
+
 INSERT INTO `usuarios` (`id` , `DNI`, `Nombre`, `username`, `Sexo`, `Numero_de_telefono`, `Tipo_de_usuario`, `password`, `Fecha_de_nacimiento`, `colegio`) VALUES(0, 0, '0', '0', 'F', 00000000, 0, '$2a$10$6lmlEuJRZ6bxbskY05sFCeyL8VZOH1L3ifJi0CQ0f0AS306QFnleq', '0000-0-00', 0);
 INSERT INTO `usuarios` (`DNI`, `Nombre`, `username`, `Sexo`, `Numero_de_telefono`, `Tipo_de_usuario`, `password`, `Fecha_de_nacimiento`, `colegio`) VALUES(1, '1', '1', 'M', 11111111, 1, '$2a$10$6lmlEuJRZ6bxbskY05sFCeyL8VZOH1L3ifJi0CQ0f0AS306QFnleq', '1111-1-11', 0), (2, '2', '2', 'F', 22222222, 2, '$2a$10$6lmlEuJRZ6bxbskY05sFCeyL8VZOH1L3ifJi0CQ0f0AS306QFnleq', '2222-2-22', 0),  (3, '3', '3', 'F', 33333333, 3, '$2a$10$6lmlEuJRZ6bxbskY05sFCeyL8VZOH1L3ifJi0CQ0f0AS306QFnleq', '3333-3-3', 0), (4, '4', '4', 'M', 44444444, 4, '$2a$10$6lmlEuJRZ6bxbskY05sFCeyL8VZOH1L3ifJi0CQ0f0AS306QFnleq', '4444-4-4', 0), (5, '5', '5', 'F', 55555555, 5, '$2a$10$6lmlEuJRZ6bxbskY05sFCeyL8VZOH1L3ifJi0CQ0f0AS306QFnleq', '5555-5-5', 0), (6, '6', '6', 'M', 66666666, 6, '$2a$10$6lmlEuJRZ6bxbskY05sFCeyL8VZOH1L3ifJi0CQ0f0AS306QFnleq', '6666-6-6', 0);
 INSERT INTO `inasistencias`(`tipo`, `motivo`, `cantidad`, `fecha`, `id_us`, `id_creador`) VALUES (1, "Tarde por ir al medico", 0, "2022-6-4", 6, 3), (0,"Inasistencia total", 1, "2022-5-23", 6, 3);
 INSERT INTO `colegio`(`pago`,`superusuario`) VALUES (1,0);
 INSERT INTO `superusuarios`(`id`,`fecha_creacion`) VALUES (0,"2012-2-15");
+UPDATE `alumno` SET  `ID Curso`= 1, `Padre`=5 WHERE `ID` = 6;  
