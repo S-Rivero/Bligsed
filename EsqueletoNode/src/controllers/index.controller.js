1c39dfd53d962b88344b1acb099f1e6319ba5486
S-Rivero <santiarivero@gmail.com>
2022-08-04 17:01:34 -0300

    Mensajeria con detalles nasheee

--- EsqueletoNode/src/controllers/index.controller.js
+++ EsqueletoNode/src/controllers/index.controller.js
@@ -20,16 +20,20 @@ exports.renderHome = ((req,res) => { //Actualmente muestra publicaciones nada ma
 });
 
 exports.renderChat = ((req,res) => {
-    const cols = pool.query("SELECT * FROM chats WHERE id_usuario = ?", req.user[0].id, function(err, chats){
+    const rows1 = pool.query("SELECT * FROM chats WHERE id_usuario = ?", req.user[0].id, function(err, chats){
         if (req.query.id_chat != null) {
-            const rows = pool.query("SELECT * FROM mensajes WHERE chatroom = ?", [req.query.id_chat], function(err, mensajes){
+            const rows2 = pool.query("SELECT * FROM mensajes WHERE chatroom = ?", [req.query.id_chat], function(err, mensajes){
                 for(let i = 0; i < mensajes.length; i++){
                     mensajes[i].userid = req.user[0].id;
                 }
-                res.render('mensajes.hbs', {layout: 'chatLY.hbs', chats, mensajes, user:req.user[0], nombre_chat_sel: req.query.nombre_chat});
+                const rows3 = pool.query("SELECT * FROM chats WHERE id_chat = "+req.query.id_chat+" AND id_usuario = "+req.user[0].id, function(err, selected_chat){
+                    const rows4 = pool.query("SELECT COUNT(id_chat) as id_chat FROM chats WHERE id_chat="+req.query.id_chat, function(err, count){
+                        res.render('mensajes.hbs', {layout: 'chatLY.hbs', chats, mensajes, user:req.user[0], selected_chat, count});
+                    });
+                });
             });
         }
         else 
-            res.render('mensajes.hbs', {layout: 'chatLY.hbs', chats, mensajes: null, user:req.user[0], nombre_chat_sel: req.query.nombre_chat});
+            res.render('mensajes.hbs', {layout: 'chatLY.hbs', chats, mensajes: null, user:req.user[0]});
     });    
 });
