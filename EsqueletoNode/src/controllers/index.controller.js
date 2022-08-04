f6095f0c137c15de76f01b8b25c59ba7ec67e0a7
S-Rivero <santiarivero@gmail.com>
2022-08-04 16:17:30 -0300

    MENSAJERIA FUNCA

--- EsqueletoNode/src/controllers/index.controller.js
+++ EsqueletoNode/src/controllers/index.controller.js
@@ -12,6 +12,7 @@ exports.root = ((req,res) => {
         res.redirect('/home');
     });
 });
+
 exports.renderHome = ((req,res) => { //Actualmente muestra publicaciones nada mas
     const rows = pool.query("SELECT * FROM publicaciones p JOIN usuarios u ON p.autor = u.id", function(err, publicaciones){
         res.render('publicaciones.hbs', {pub: publicaciones, links: 'headerLinks/home', user:req.user[0]});
@@ -19,19 +20,16 @@ exports.renderHome = ((req,res) => { //Actualmente muestra publicaciones nada ma
 });
 
 exports.renderChat = ((req,res) => {
-    const rows = pool.query("SELECT * FROM mensajes WHERE chatroom = ?", [req.body.chat_id], function(err, mensajes){
-        for(let i = 0; i < mensajes.length; i++){
-            mensajes[i].userid = req.user[0].id;
+    const cols = pool.query("SELECT * FROM chats WHERE id_usuario = ?", req.user[0].id, function(err, chats){
+        if (req.query.id_chat != null) {
+            const rows = pool.query("SELECT * FROM mensajes WHERE chatroom = ?", [req.query.id_chat], function(err, mensajes){
+                for(let i = 0; i < mensajes.length; i++){
+                    mensajes[i].userid = req.user[0].id;
+                }
+                res.render('mensajes.hbs', {layout: 'chatLY.hbs', chats, mensajes, user:req.user[0], nombre_chat_sel: req.query.nombre_chat});
+            });
         }
-        res.render('chat.hbs', {layout: 'mensajeriaPrueba.hbs', message: mensajes, user:req.user[0]});
-    });
-});
-
-exports.renderChatPrueba = ((req,res) => {
-    const rows = pool.query("SELECT * FROM mensajes WHERE chatroom = 1", function(err, mensajes){
-        for(let i = 0; i < mensajes.length; i++){
-            mensajes[i].userid = req.user[0].id;
-        }
-        res.render('chat.hbs', {layout: 'chatLY.hbs', message: mensajes, user:req.user[0]});
-    });
+        else 
+            res.render('mensajes.hbs', {layout: 'chatLY.hbs', chats, mensajes: null, user:req.user[0], nombre_chat_sel: req.query.nombre_chat});
+    });    
 });
