cb7e0af0b2aeaa1b830dbd3bd61bce3ce1ffe92c
S-Rivero <santiarivero@gmail.com>
2022-08-09 01:38:19 -0300

    Idea basica publicaciones funcionales

---
+++ EsqueletoNode/src/public/js/pub.js
@@ -0,0 +1,11 @@
+//"INSERT INTO publicaciones (titulo, descripcion, autor, fecha) VALUES ('"+req.body.title+"', '"+req.body.desc+"', '"+req.body.autName+"', '"+req.body.date+"');"
+
+/*
+<form id="form_data" action="/msg" method="post" >
+    <input type="text" name="title" id="title" placeholder="Título...">
+    <input type="text" name="desc" id="desc" placeholder="Descripción...">
+    <input type="text" name="autName" id="autName" value="{{user.Nombre}}" style="display: none;">
+    <input type="text" name="date" id="date" value="" style="display: none;">
+    <input type="submit" id="submit" value="Enviar"/>
+</form>
+*/
