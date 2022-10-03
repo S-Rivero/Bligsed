6d42672dec705e0a75689f2dce84c8ed8902be84
Nasher3296 <ignaciofernandez3296@gmail.com>
2022-10-03 15:00:06 -0300

    monger donger

--- EsqueletoNode/src/lib/jsonFormat.js
+++ EsqueletoNode/src/lib/jsonFormat.js
@@ -2,38 +2,27 @@ const e = require("connect-flash");
 
 module.exports = {
     JSONPromediosAl: function(json){
-        let formateado = {};
-        let materias = {};
-        let max = {};
-        let maxNotas = 0;
-        let contNotas = 0;
-        let maxMateria = '';
-        let band = 0;
-        json.forEach(j => {
-            if(!(j.Materia in materias)){
-                contNotas = 0;
-                materias[j.Materia] = [];
-                band++;
+        let materias = [];
+        json.forEach(e =>{
+            if(!(materias.includes(e.Materia))){
+                materias.push(e.Materia);
             }
-            contNotas++;
-            materias[j.Materia].push(j.nota);
-            if(band == 1){
-                maxMateria = j.Materia;
-                maxNotas = contNotas;
-            }else{
-                if(contNotas > maxNotas){
-                    maxNotas = contNotas;
-                    maxMateria = j.Materia;
-                }
-            }
-        });
-        formateado['materias'] = materias;
-        max['materia'] = maxMateria;
-        max['cantidad'] = maxNotas;
-        formateado['max'] = max;
-        return formateado;
-    },
+        })
+        
+        return materias.map(m => {
+            let notas = [];
+            json.forEach(e => {
+                if(e.Materia == m)
+                    notas.push(e.nota);
+            });
+            return{
+                "materia": m,
+                "notas": notas
+            };
+        })
 
+    },
+    
     JSONListaDeCursos: function(arrJson){
         let cursos = []; 
         let anios = [];
