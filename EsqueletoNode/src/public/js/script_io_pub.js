8ec116588f34b873cc5ba8e70c654731a4bf62db
Gonxa6282 <gonzaloescobar121@gmail.com>
2022-10-06 14:58:01 -0300

    I give up
    Co-Authored-By: S-Rivero <70220211+S-Rivero@users.noreply.github.com>
    Co-Authored-By: MendezLucas <70654158+MendezLucas@users.noreply.github.com>

--- EsqueletoNode/src/public/js/script_io_pub.js
+++ EsqueletoNode/src/public/js/script_io_pub.js
@@ -15,26 +15,23 @@ socket.on('newPub', arr => {
 
 function printPub(e) {
     // title1 «¯§¦Æ×þ®©©» desc1 «¯§¦Æ×þ®©©» name «¯§¦Æ×þ®©©» date «¯§¦Æ×þ®©©» file1,file2,no
-    let pub       = document.createElement('li'),
+    let pub       = document.createElement('div'),
         autor     = document.createElement('div'),
         aNombre   = document.createElement('div'),
         aFecha    = document.createElement('div'),
         contenido = document.createElement('div'),
         cTitle    = document.createElement('div'),
-        cText     = document.createElement('div'),
-        cVer      = document.createElement('div'),
-        caVer     = document.createElement('a'  );
+        cText     = document.createElement('div');
 
-    pub       .style     = "list-style: none";
-    autor     .className = 'publicaciones_autor';
+    pub       .className = 'publicacion';
+    autor     .className = 'publicacion_autor';
     aNombre   .className = 'autor_nombre';
     aFecha    .className = 'autor_fecha';
-    contenido .className = 'publicaciones_contenido';
+    contenido .className = 'publicacion_contenido';
     cTitle    .className = 'contenido_titulo';
     cText     .className = 'contenido_texto';
-    cVer      .className = 'contenido_vermas';
-    caVer     .className = 'boton_vermas';
-    if(/\n/.test(e[1])){
+
+    if (/\n/.test(e[1])) {
         e[1] = e[1].split('\n');
         e[1].forEach(e => {
             p = document.createElement('p');
@@ -45,12 +42,11 @@ function printPub(e) {
         })
     }
     else
-        cText.textContent  = e[1];
-        
-    caVer.textContent   = 'Ver más';
-    aFecha.textContent  = e[3];
+        cText.textContent = e[1];
+
+    aFecha.textContent = e[3];
     aNombre.textContent = e[2];
-    cTitle.textContent  = e[0];
+    cTitle.textContent = e[0];
 
     if (e[4] != 'no') {
         cFiles = document.createElement('div');
@@ -65,14 +61,13 @@ function printPub(e) {
         })
         cText.appendChild(cFiles);
     }
-    
+
     autor.appendChild(aNombre);
     autor.appendChild(aFecha);
-    cVer.appendChild(caVer);
     contenido.appendChild(cTitle);
     contenido.appendChild(cText);
-    contenido.appendChild(cVer);
     pub.appendChild(autor);
     pub.appendChild(contenido);
     ul.insertAdjacentElement("afterbegin", pub);
+    z++;
 }
