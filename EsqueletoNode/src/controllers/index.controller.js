a35217aa6b1604b3a37d44372a4484699a2532be
Nasher3296 <ignaciofernandez3296@gmail.com>
2022-10-12 14:59:15 -0300

    boludez xd

--- EsqueletoNode/src/controllers/index.controller.js
+++ EsqueletoNode/src/controllers/index.controller.js
@@ -213,44 +213,6 @@ exports.insertCuentas = (async(req, res) => {
     let tdu = req.params.tipo;
     let arr = [];
     let usernames = req.body.username;
-    if(tdu == 6){
-        for(let i = 0 ; i < req.body.username.length ; i++){
-            let aux = [];
-            Object.values(req.body).forEach(e => {
-                aux.push(e[i]);
-            });
-            let passNoHash = RandomString(8);
-            let hashPass = await encryptPassword(passNoHash);
-            aux.push(hashPass);
-            
-            arr.push(aux);
-        }
-    }else{
-        arr = Object.values(req.body);
-        let passNoHash = RandomString(8);
-        let hashPass = await encryptPassword(passNoHash);
-        arr.push(hashPass);
-    }
-    pool.query(`
-        SELECT username
-        FROM usuarios
-        WHERE username = ?`
-        , [usernames], function(err, a){
-            if(!(a[0])){
-
-                pool.query(`
-                    INSERT INTO usuarios
-                    (username, nombre, dni, Sexo, Fecha_de_nacimiento, Numero_de_telefono, domicilio, Tipo_de_usuario, password)
-                    VALUES (?)`
-                    ,[arr], function(err, b){
-                        if(err)
-                            res.send(err)
-                        else
-                         res.redirect('/crear_cuentas');
-                    }
-                )
-            }}
-        );
     
 });
 
