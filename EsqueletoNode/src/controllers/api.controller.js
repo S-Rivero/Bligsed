65e742886a1f38626ef065d2ac050329188dd930
Nasher3296 <ignaciofernandez3296@gmail.com>
2022-10-24 16:02:05 -0300

    ya se borra curso xd

--- EsqueletoNode/src/controllers/api.controller.js
+++ EsqueletoNode/src/controllers/api.controller.js
@@ -206,3 +206,11 @@ exports.actualizarAlumno = (async(req, res) => {
         });
     }
 });
+
+
+exports.eliminarCursos = ((req,res) => {
+    let cursos = Object.keys(req.body);
+    pool.query(`DELETE FROM curso WHERE ID IN (?);`, cursos, function(err,a){
+        res.redirect('/buscarCurso');
+    });
+});
