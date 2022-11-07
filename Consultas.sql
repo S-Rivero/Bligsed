6011b6f88abcd8975e61e554aa8647df0eee42a9
S-Rivero <santiarivero@gmail.com>
2022-11-07 13:29:10 -0300

    Limpieza

--- Consultas.sql
+++
@@ -1,5 +0,0 @@
---Baja todas las publicaciones en el orden que estas fueron creadas/cargadas--
-SELECT * FROM `publicaciones` order by id DESC;
-
-
---Hagan como comentario la peticion de el tipo de consulta que necesitan-
--- EsqueletoNode/consultas profesores.sql
+++
@@ -1,9 +0,0 @@
---Obtener todas las materias de un profesor
-SELECT ID, Materia FROM `materias` WHERE profesor = 4;
-
---Obtener todos los cursos de un profesor
-SELECT c.Nombre_curso as Curso, Mat.ID as IdMateria, Mat.Materia as Materia FROM curso c INNER JOIN (SELECT ID, Materia, IdCurso FROM `materias` WHERE profesor = 4) Mat ON c.ID = Mat.IdCurso GROUP BY Curso;
---Obtener todos los id de alumnos de un curso
--- SELECT a.id as idAlumno FROM alumno a 
--- JOIN (SELECT c.Nombre_curso as Curso, Mat.ID as IdMateria, Mat.IdCurso as IdCurso, Mat.Materia as Materia FROM curso c INNER JOIN (SELECT ID, Materia, IdCurso FROM `materias` WHERE profesor = 4) Mat ON c.ID = Mat.IdCurso) Cur ON Cur.IdCurso = a.ID_Curso;
-SELECT a.id as idAlumno FROM alumno a JOIN curso c ON a.ID_Curso = c.ID WHERE c.ID = 1;
